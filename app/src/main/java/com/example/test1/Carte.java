8e88cfddc913d60f483b81a51bdab0d577dd59b7
FlorentDuboquet <florent.duboquet@student.umons.ac.be>
2019-05-21 22:51:57 +0200

    add DB + Aff table produit

--- app/src/main/java/com/example/test1/Carte.java
+++ app/src/main/java/com/example/test1/Carte.java
@@ -1,14 +1,23 @@
 package com.example.test1;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Carte extends AppCompatActivity {
 
     private Button commande;
+    private SQLiteDatabase db;
+    private Cursor req;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,32 @@ public class Carte extends AppCompatActivity {
                 Intent otherActivity = new Intent (getApplicationContext(), Commande.class);
                 startActivity(otherActivity);
                 finish();
+
+                db = openOrCreateDatabase("App.db", MODE_PRIVATE,null);
+                req = db.rawQuery("SELECT * FROM Tache ORDER BY type", null);
+
+
+                List<Element> elementList = new ArrayList<>();
+
+                if(req != null && req.getCount()>0)
+
+                {
+                    while(req.moveToNext())
+                    {
+                        elementList.add(new Element(req.getInt(req.getColumnIndex("id")),req.getString(req.getColumnIndex("actions")),req.getString(req.getColumnIndex("date")),req.getString(req.getColumnIndex("duree"))));
+                    }
+
+                    db.close();
+                    ListView elementListView = findViewById(R.id.carte_list);
+                    elementListView.setAdapter(new ElementAdapter(this, elementList));
+
+                }
+
+
+
+
+                db.close();
+
 
             }
         });
--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.test1;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +11,24 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView accueil;
+    private static final String DB_NAME = "App.db";
+    private static final String TABLE_NAME1 = "produit";
+    private static final String TABLE_NAME2= "menu";
+    private static final String TABLE_NAME3 = "panier";
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = openOrCreateDatabase(DB_NAME, Context.MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_NAME1 + "(id1 INTEGER PRIMARY KEY AUTOINCREMENT, " + "nom TEXT,type TEXT, temperature TEXT, prix int);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_NAME2 + "(id2 INTEGER PRIMARY KEY AUTOINCREMENT, " + "Entrée TEXT,plat TEXT, dessert TEXT, prix int);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_NAME3 + "(id3 INTEGER PRIMARY KEY AUTOINCREMENT, " + "nom TEXT, prix int);");
+
+        db.close();
+
 
         this.accueil = (ImageView) findViewById(R.id.accueil);
         accueil.setOnClickListener(new View.OnClickListener()
